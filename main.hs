c7e13c760964707d100bacb48cfd26c6af536d85
alex <amurshak@gmail.com>
2023-06-25 22:47:12 -0500

    Initial commit

---
+++ main.hs
@@ -0,0 +1,12 @@
+-- Gradient Descent for linear regression
+main :: IO ()
+main = putStrLn "Hello, World!"
+-- yhat = wx+b
+-- losss = (y-yhat)**2 / N
+
+-- Initiaize parameters
+
+-- Create gradient descent function
+ 
+ -- Iteratively make updates
+
